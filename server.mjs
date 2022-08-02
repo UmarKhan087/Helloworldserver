0239327b9183712087ee66f60adc826e72d9fa77
unknown <mumarkhan087@gmail.com>
2022-08-02 21:28:03 +0500

    heroku portnumber

--- server.mjs
+++ server.mjs
@@ -1,9 +1,13 @@
 import express from 'express'
 const app = express()
-const port = 3000
+const port =process.env.port|| 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/weather', (req, res) => {
+  res.send('My weather app!')
 })
 
 app.listen(port, () => {
